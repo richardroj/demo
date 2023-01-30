a2bd58f95339cc9e13a280a0d49c94179db34f1f
Richard <richardroj@gmail.com>
2023-01-30 21:03:24 +0100

    Update greeting hello

--- hello-cloudbuild-v2/main.go
+++ hello-cloudbuild-v2/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, Richard from google")
+	fmt.Fprintln(w, "Hola, Richard from google")
 }
 
 func main() {
